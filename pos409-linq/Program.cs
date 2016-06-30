9bf9917147f4dbeefa02c52146232f767bd104f0
Gordon Doskas <gordon2012@users.noreply.github.com>
2016-06-30 09:33:14 -0700

    read text file into a list

--- pos409-linq/Program.cs
+++ pos409-linq/Program.cs
@@ -1,8 +1,35 @@
-﻿using System;
+﻿/*
+
+ [X] The first step is to read a text file into the program. The file should be a list of cities and state abbreviations. Each line should look as follows: Chicago, IL. Read each
+ city/state into a list. You do not need to separate the city and state into separate fields. As you read each line of the file, count up how many total cities are in the file
+ and then present the total to the end user.
+ 
+The next step is to develop a set of queries and present the results to the end user. In total you will do five Linq queries:
+
+ [ ] Execute a Linq query to count how many records are in the list and present the total to the end user.
+
+ [ ] Execute a Linq query to sort the entries in the list in ascending order and present the list of cities to the end user, in order.
+
+ [ ] Execute a Linq query to find all states starting with a specific letter. Prompt the user for the letter they want to look for, then present those that start with that
+ letter to them. There is no need at this time to validate that they entered a letter.
+
+ [ ] Execute a Linq query to find all cities that are from a given state. Prompt the user for the state they want to look for, then present those that are from that state to
+ them. Again there is no need to validate that they entered a valid state.
+
+ [ ] Last, design your own Linq query and present the results to the end user. Be creative!
+
+ [ ] Please note that you may need to do some research on MSDN to find Linq methods to handle some of the queries. In addition, you are not going to find answers by searching
+ for things like "how to handle user input with Linq". You are going to need to use your analysis skills to figure out the steps to accomplish these requirements using logic,
+ the skills you have built over the last few weeks, and what you have learned about Linq.
+
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using System.IO;
 
 namespace pos409_linq
 {
@@ -10,6 +37,36 @@ namespace pos409_linq
     {
         static void Main(string[] args)
         {
+            List<string> cities = new List<string>();
+            
+            int count = 0;
+
+            try
+            {
+                using (StreamReader file = new StreamReader("input.txt"))
+                {
+                    string line;
+                    while (!file.EndOfStream)
+                    {
+                        line = file.ReadLine();
+                        if (!line.Equals(""))
+                        { 
+                            cities.Add(line);
+                            count++;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.GetType().ToString() + ": " + ex.Message);
+            }
+
+            cities.ForEach(city => Console.WriteLine(city));
+
+            Console.WriteLine(count);
+
+            Console.ReadLine();
         }
     }
 }
